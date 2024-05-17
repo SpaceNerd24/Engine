a5d90172cb75c605d1e351451bea590afa3b41d1
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2024-05-17 13:15:46 +0000

    stuff

--- example.js
+++ example.js
@@ -9,37 +9,45 @@ window.onload = function() {
     let movementSpeed = 2;
 
     // Create a new sprite
-    const sprite = new Sprite("assets/images/sprite.png", 32, 32, 100, 100, 'Player');
+    const player = new Sprite("assets/images/sprite.png", 32, 32, 100, 100, 'Player');
+    const PlayerCollision = new Sprite('assets/images/CollisionTest.png', 16 , 16, 100, 100, 'PlayerCollision');
     const CollisionTest = new Sprite('assets/images/CollisionTest.png', 32 , 32, 100, 50, 'Collision Test');
+    const CollisionTest2 = new Sprite('assets/images/CollisionTest.png', 32 , 32, 50, 100, 'Collision Test2');
 
-    collisionManager.addEntity(sprite);
+    //collisionManager.addEntity(sprite);
+    collisionManager.addEntity(PlayerCollision);
     collisionManager.addEntity(CollisionTest);
+    collisionManager.addEntity(CollisionTest2);
 
     inputManager.bindKey("p", () => {
         document.getElementById('id01').style.display='block'
     })
 
     inputManager.bindKey("w", () => {
-        sprite.translate(0, -movementSpeed);
+        player.translate(0, -movementSpeed);
     });
 
     inputManager.bindKey("s", () => {
-        sprite.translate(0, movementSpeed);
+        player.translate(0, movementSpeed);
     });
 
     inputManager.bindKey("a", () => {
-        sprite.translate(-movementSpeed, 0);
+        player.translate(-movementSpeed, 0);
     })
 
     inputManager.bindKey("d", () => {
-        sprite.translate(movementSpeed, 0);
+        player.translate(movementSpeed, 0);
     });
 
     function gameLoop() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        sprite.draw(ctx);
+        player.draw(ctx);
         CollisionTest.draw(ctx);
+        CollisionTest2.draw(ctx);
+        //PlayerCollision.draw(ctx);
+
+        PlayerCollision.setPosition(player.posX + 8, player.posY + 8);
 
         collisionManager.checkAllCollisions();
 
