57cdf684cb065a3c821adb7d18949245e7b34723
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2024-05-17 15:50:47 +0000

    Ws in the chat

--- example.js
+++ example.js
@@ -10,12 +10,12 @@ window.onload = function() {
 
     // Create a new sprite
     const player = new Sprite("assets/images/sprite.png", 32, 32, 100, 100, 'Player');
-    const PlayerCollision = new Sprite('assets/images/CollisionTest.png', 16 , 16, 100, 100, 'PlayerCollision');
+    const PlayerHitbox = new Sprite('assets/images/CollisionTest.png', 16 , 16, 100, 100, 'PlayerHitbox');
     const CollisionTest = new Sprite('assets/images/CollisionTest.png', 32 , 32, 100, 50, 'Collision Test');
     const CollisionTest2 = new Sprite('assets/images/CollisionTest.png', 32 , 32, 50, 100, 'Collision Test2');
 
     //collisionManager.addEntity(sprite);
-    collisionManager.addEntity(PlayerCollision);
+    collisionManager.addEntity(PlayerHitbox);
     collisionManager.addEntity(CollisionTest);
     collisionManager.addEntity(CollisionTest2);
 
@@ -45,9 +45,9 @@ window.onload = function() {
         player.draw(ctx);
         CollisionTest.draw(ctx);
         CollisionTest2.draw(ctx);
-        //PlayerCollision.draw(ctx);
+        //PlayerHitbox.draw(ctx); Does not need to be rendered since its a htibox
 
-        PlayerCollision.setPosition(player.posX + 8, player.posY + 8);
+        PlayerHitbox.setPosition(player.posX + 8, player.posY + 8);
 
         collisionManager.checkAllCollisions();
 
