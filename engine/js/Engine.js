4cd849c49443f23062291bb6b5a525a06b7a38d5
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2024-05-20 16:19:35 +0000

    minor fix

--- engine/js/Engine.js
+++ engine/js/Engine.js
@@ -110,3 +110,11 @@ setInterval(() => {
         document.getElementById('id01').style.display='block'
     }
 }, 1);
+
+var modal = document.getElementById('id01');
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
